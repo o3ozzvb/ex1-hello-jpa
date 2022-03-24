8502046698868c69ca2236f665313bd6c5eb2551
oeunkyoung <ou1112@naver.com>
2022-03-24 12:26:59 +0900

    영속성 관리

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,35 +16,49 @@ public class JpaMain {
         tx.begin();
 
         try {
-            /* 회원정보 등록
+            /*
+            //비영속
             Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            member.setId(101L);
+            member.setName("HelloJPA");
 
-록           em.persist(member);
+            //영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER  ===");
+            */
+            /*
+            Member findMember1 = em.find(Member.class, 101L);
+            Member findMember2 = em.find(Member.class, 101L);
+
+            //System.out.println("findMember.getId() = " + findMember.getId());
+            //System.out.println("findMember.getName() = " + findMember.getName());
+            System.out.println("(findMember1 == findMember2) : " + (findMember1 == findMember2));
+            */
+            /*
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+            */
+            /*
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
             */
 
-            /* 회원정보 수정
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
-            */
+            Member member = new Member(200L, "member200");
+            em.persist(member);
 
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
+            em.flush();
 
-            for(Member member : result) {
-                System.out.println("member = " + member.getName());
-            }
-
+            System.out.println("==================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
             em.close();
         }
-
         emf.close();
     }
 }
