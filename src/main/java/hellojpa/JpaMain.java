275b70e6bf30e06b6700bf6c78afae6060385aa9
oeunkyoung <ou1112@naver.com>
2022-04-11 22:34:53 +0900

    즉시 로딩과 지연 로딩

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,29 +16,34 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
+
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-//            refMember.getUsername(); //Proxy 강제 초기화
+//            Member m = em.find(Member.class, member1.getId());
 
-//            프록시 인스터스의 초기화 여부 확인
-//            System.out.println("isLoaded = " + emf.getPersistenceUnitUtil().isLoaded(refMember));
-
-//            프록시 강제 초기화
-            Hibernate.initialize(refMember);
-
-//            Member findMember = em.find(Member.class, member1.getId());
-//            System.out.println("findMember = " + findMember.getClass());
-//
-//            System.out.println("a == a : " + (refMember == findMember));
-
-//            System.out.println("m1 == m2 : " + (m1 == m2));
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
+            // SQL : select * from Member;
+            // SQL : select * from Member where TEAM_ID = ?
 
             tx.commit();
         } catch (Exception e) {
